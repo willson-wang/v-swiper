37fbca9128363421b90ecffae2a9df0a7f8da718
wangks <wangkangsen168@163.com>
2019-03-24 19:00:11 +0800

    新的思路实现轮播

--- src/lib/v-swiper/lib/index.vue
+++ src/lib/v-swiper/lib/index.vue
@@ -1,24 +1,16 @@
 <template>
-    <div class="swiper-wrap" ref="swiper" :class="className" :style="{ height }">
-        <div
-            class="swiper-content clearfix"
-            ref="swiperContent"
-            :style="styleObj"
-            :class="[isTransition ? 'swiper-transition' : '']"
-        >
+    <div class="swiper-wrap" ref="swiper" :class="className">
+        <div class="swiper-content" ref="swiperContent" :style="{ height }">
             <div
                 v-for="(item, index) in newList"
                 class="swiper-item"
+                :class="[index === 1 && 'active']"
                 :key="index"
+                :data-offset="offset[index]"
                 @click="selectItem(item)"
             >
-                <img
-                    v-if="!isBroadcast"
-                    :src="item.img"
-                    :alt="'banner' + index"
-                    :style="{ width, height }"
-                />
-                <div class="swiper-txt" v-else :style="{ width, height, lineHeight: height }">
+                <img v-if="!isBroadcast" :src="item.img" :alt="'banner' + index" />
+                <div class="swiper-txt" v-else :style="{ height, lineHeight: height }">
                     {{ item.txt }}
                 </div>
             </div>
@@ -27,7 +19,7 @@
             <span
                 v-for="(item, index) in list"
                 :key="index"
-                :class="[index === fixIndex ? 'active' : '']"
+                :class="[index === currentIndex ? 'active' : '']"
             ></span>
         </div>
         <div class="swiper-prev" v-if="isPrevShow" @click="changeItem('prev')">{{ lt }}</div>
@@ -36,9 +28,9 @@
 </template>
 
 <script>
-import Swiper from './swiper'
+// import Swiper from './swiper'
 export default {
-    name: 'swiper',
+    name: 'slide',
     props: {
         list: {
             type: Array,
@@ -78,14 +70,6 @@ export default {
             type: Number,
             default: 4000
         },
-        animateInterval: {
-            type: Number,
-            default: 20
-        },
-        isTransition: {
-            type: Boolean,
-            default: true
-        },
         minMovingDistance: {
             type: Number,
             default: 30
@@ -116,123 +100,275 @@ export default {
         }
     },
     computed: {
-        newList() {
-            if (this.loop && this.list.length) {
-                const tempArr = JSON.parse(JSON.stringify(this.list))
-                tempArr.unshift(this.list[this.list.length - 1])
-                tempArr.push(this.list[0])
-                return tempArr
-            }
-            return this.list
+        newHeight() {
+            return this.height
         },
-        styleObj() {
-            const transformx = this.direction === 'horizontal' ? this.transformx : 0
-            const transformy = this.direction === 'horizontal' ? 0 : this.transformx
-            const left = this.direction === 'horizontal' ? this.left : 0
-            const top = this.direction === 'horizontal' ? 0 : this.left
-            const width =
-                this.direction === 'horizontal'
-                    ? parseInt(this.width) * this.newList.length + 'px'
-                    : '100%'
-            return {
-                width,
-                transform: `translate3d(${transformx}, ${transformy}, 0)`,
-                transitionDuration: `${this.newDuration}ms`,
-                left: left + 'px',
-                top: top + 'px'
+        newList() {
+            const tempArr = JSON.parse(JSON.stringify(this.list))
+            if (this.list.length !== 2) {
+                const temp = tempArr.pop()
+                tempArr.unshift(temp)
             }
+            console.log(tempArr, this.list)
+            this.setOffset(tempArr)
+            return tempArr
+        },
+        onlyOne() {
+            return this.list.length === 1
+        },
+        onlyTwo() {
+            return this.list.length === 2
         }
     },
     data() {
-        let height = this.remHeightToPx()
-        const initTransform =
-            this.direction === 'horizontal'
-                ? this.initWidth || window.innerWidth
-                : parseFloat(height)
-        const width =
-            this.direction === 'horizontal' ? this.initWidth || window.innerWidth + 'px' : '100%'
+        const width = document.documentElement.offsetWidth
         return {
+            currentIndex: 0,
             width,
-            currentIndex: this.loop ? 1 : 0,
-            transformx: this.loop && this.isTransition ? `-${initTransform}px` : 0,
-            fixIndex: 0,
-            newDuration: this.isTransition ? this.duration : 0,
-            left: this.loop && !this.isTransition ? `-${initTransform}` : 0,
-            lt: '&lt;',
-            gt: '&gt;'
+            slideDerection: null,
+            touchStartInfo: {},
+            touchMoveInfo: {},
+            touchEndInfo: {},
+            isTouchMoving: false,
+            isGoing: false,
+            offset: []
         }
     },
     watch: {
-        list: {
-            handler(val) {
-                this.$nextTick(() => {
-                    !this.swiper ? this.initSwiper() : this.auto && this.swiper.autoPlay()
-                })
-            },
-            immediate: true
+        currentIndex(val) {
+            this.$emit('input', val)
         }
     },
     methods: {
-        changeItem(key) {
-            this.swiper.move(key)
+        next() {
+            this.slideDerection = 'nexted'
+            this.currentIndex += 1
+            this.go(this.currentIndex)
         },
-        selectItem(item) {
-            this.$emit('selectItem', { ...item, currentIndex: this.fixIndex })
+        prev() {
+            this.slideDerection = 'preved'
+            this.currentIndex -= 1
+            this.go(this.currentIndex)
         },
-        initSwiper() {
-            this.swiper = new Swiper(this.$refs.swiper, {
-                auto: this.auto,
-                loop: this.loop,
-                data: this.newList,
-                isTransition: this.isTransition,
-                direction: this.direction,
-                height: this.height,
-                width: this.width,
-                interval: this.interval,
-                forbidTouchStart: this.forbidTouchStart,
-                on: res => {
-                    this.transformx = res.transformx
-                    this.newDuration = res.newDuration
-                    this.fixIndex = res.fixIndex
-                    this.left = res.left
-                    this.currentIndex = res.currentIndex
-                },
-                end: res => {
-                    this.$emit('annimateEnd', res)
-                },
-                resize: () => {
-                    this.recomputed()
+        go(idx) {
+            this.isGoing = true
+            if (this.loop) {
+                if (idx > this.list.length - 1) {
+                    if (this.list.length === 2) {
+                        this.currentIndex = this.list.length - 1
+                        this.isGoing = false
+                        return
+                    }
+                    this.currentIndex = 0
                 }
+                if (idx < 0) {
+                    if (this.list.length === 2) {
+                        this.currentIndex = 0
+                        this.isGoing = false
+                        return
+                    }
+                    this.currentIndex = this.list.length - 1
+                }
+            } else {
+                // 无缝轮播索引大于当前最大值及小于0时，禁止切换到下一张
+                if (idx > this.list.length - 1) {
+                    this.currentIndex = this.list.length - 1
+                    this.isGoing = false
+                    return
+                }
+                if (idx < 0) {
+                    this.currentIndex = 0
+                    this.isGoing = false
+                    return
+                }
+            }
+            this.forItems((item, key) => {
+                console.log('this.duration', this.duration)
+                this.setTransition(item, this.duration)
+                let distance =
+                    this.slideDerection === 'nexted'
+                        ? this.offset[key] - this.width
+                        : this.offset[key] + this.width
+                console.log()
+                this.setTransform(item, distance)
             })
         },
-        recomputed() {
-            const rect = this.$refs.swiper && this.$refs.swiper.getBoundingClientRect()
-            let height = this.remHeightToPx()
-            this.width = rect.width + 'px'
-            const initTransform = this.direction === 'horizontal' ? rect.width : parseFloat(height)
-            this.transformx = this.loop && this.isTransition ? `-${initTransform}px` : 0
-            this.left = this.loop && !this.isTransition ? `-${initTransform}` : 0
+        transitionEndHandler(e) {
+            if (this.list.length !== 2) {
+                this.forItems((item, key) => {
+                    this.setTransition(item)
+                    let distance = this.offset[key]
+                    this.setTransform(item, distance)
+                })
+                const temp = this.slideDerection === 'nexted' ? this.list.shift() : this.list.pop()
+                this.slideDerection === 'nexted' ? this.list.push(temp) : this.list.unshift(temp)
+            } else {
+                this.setOffset(this.list)
+            }
+            this.isGoing = false
+            console.log('transitionEndHandler')
         },
-        remHeightToPx() {
-            let htmlFontSize =
-                window.getComputedStyle(document.documentElement).fontSize ||
-                (document.documentElement.currentStyle &&
-                    document.documentElement.currentStyle['font-size']) ||
-                '75px'
-            let reg = /rem/gi
-            return reg.test(this.height)
-                ? parseFloat(this.height) * parseInt(htmlFontSize)
-                : this.height
+        checkTransitionEvent() {
+            const el = document.createElement('div')
+            const transitions = {
+                transition: 'transitionend',
+                OTransition: 'oTransitionEnd',
+                MozTransition: 'transitionend',
+                WebkitTransition: 'webkitTransitionEnd'
+            }
+
+            for (let t in transitions) {
+                if (el.style[t] !== undefined) {
+                    return transitions[t]
+                }
+            }
+        },
+        setCountStep(index) {
+            const currentIndex = this.currentIndex
+            console.log('setCountPage', index, this.currentIndex, index - currentIndex)
+            if (index > currentIndex) {
+                const temp = this.list.splice(0, index - currentIndex)
+                this.list.push(...temp)
+            } else if (index < currentIndex) {
+                const temp = this.list.splice(index - currentIndex)
+                this.list.unshift(...temp)
+            }
+            this.currentIndex = index
+        },
+        handleTouchStart(e) {
+            console.log('handleTouchStart')
+            const touches = e.targetTouches[0]
+            this.touchStartInfo = {
+                pageX: touches.pageX,
+                pageY: touches.pageY
+            }
+        },
+        handleTouchMove(e) {
+            if (this.isGoing) return
+            this.isTouchMoving = true
+            const touches = e.targetTouches[0]
+            this.touchMoveInfo = {
+                pageX: touches.pageX,
+                pageY: touches.pageY
+            }
+            const moveDistanceX = this.touchMoveInfo.pageX - this.touchStartInfo.pageX
+            if (
+                (this.currentIndex === 0 && this.list.length === 2 && moveDistanceX >= 0) ||
+                (this.currentIndex === this.newList.length - 1 &&
+                    this.list.length === 2 &&
+                    moveDistanceX <= 0)
+            ) {
+                return
+            }
+            // 非无缝轮播时，第一张且滑动距离大于0时，及最后一张距离小于0时return，禁止移动位置
+            if (
+                (this.currentIndex === 0 && !this.loop && moveDistanceX >= 0) ||
+                (this.currentIndex === this.newList.length - 1 && !this.loop && moveDistanceX <= 0)
+            ) {
+                return
+            }
+            this.forItems(item => {
+                this.setTransition(item)
+                let distance = parseInt(item.dataset.offset, 10) + moveDistanceX
+                this.setTransform(item, distance)
+            })
+        },
+        handleTouchEnd(e) {
+            if (this.isGoing) return
+            console.log('handleTouchEnd')
+            this.isTouchMoving = false
+            const touches = e.changedTouches[0]
+            this.touchEndInfo = {
+                pageX: touches.pageX,
+                pageY: touches.pageY
+            }
+            const direction = this.touchEndInfo.pageX - this.touchStartInfo.pageX
+            if (direction > 50) {
+                this.prev()
+            } else if (direction < -50) {
+                this.next()
+            } else {
+                this.forItems(item => {
+                    this.setTransition(item)
+                    let distance = parseInt(item.dataset.offset, 10)
+                    this.setTransform(item, distance)
+                })
+            }
+        },
+        getChildrenEle() {
+            const children = (this.$refs.swiperContent && this.$refs.swiperContent.children) || []
+            return Array.from(children)
+        },
+        setTransition(item, duration = 0) {
+            if (this.onlyOne) return
+            const transition = `all ${duration}ms ease 0s`
+            item.style.webkitTransition = transition
+            item.style.transition = transition
+        },
+        setTransform(item, distance) {
+            if (this.onlyOne) return
+            let transform = `translate3d(${distance}px, 0, 0)`
+            item.style.transform = transform
+        },
+        forItems(fn) {
+            const itemEles = this.getChildrenEle()
+            itemEles.forEach(fn)
+        },
+        handleResize() {
+            clearTimeout(this.resizeTimer)
+            this.resizeTimer = setTimeout(() => {
+                console.log('handleResize')
+                this.updateWidth()
+                this.forItems((item, key) => {
+                    this.setTransition(item)
+                    let distance = this.offset[key]
+                    this.setTransform(item, distance)
+                })
+            }, 100)
+        },
+        updateWidth() {
+            this.width = this.$refs.swiper.offsetWidth || document.documentElement.offsetWidth
+        },
+        setOffset(list) {
+            if (list.length === 2) {
+                this.offset = this.list.map((item, index) => {
+                    return this.slideDerection === 'nexted'
+                        ? this.width * (index - 1)
+                        : this.width * index
+                })
+                return
+            }
+            this.offset = list.map((item, index) => {
+                return -this.width * (1 - index)
+            })
         }
     },
     mounted() {
-        this.recomputed()
+        const itemEles = this.getChildrenEle()
+        const transitionEvent = this.checkTransitionEvent()
+        itemEles[0] &&
+            itemEles[0].addEventListener(transitionEvent, this.transitionEndHandler, false)
+        this.$refs.swiper.addEventListener('touchstart', this.handleTouchStart, false)
+        this.$refs.swiper.addEventListener('touchmove', this.handleTouchMove, false)
+        this.$refs.swiper.addEventListener('touchend', this.handleTouchEnd, false)
+        window.addEventListener('resize', this.handleResize, false)
+        this.width = this.$refs.swiper.offsetWidth || document.documentElement.offsetWidth
+        console.log('mounted')
+        itemEles.forEach((item, key) => {
+            if (this.onlyOne) return
+            console.log(this.offset)
+            let distance = this.offset[key]
+            this.setTransform(item, distance)
+        })
     },
     deactivated() {
-        this.swiper && this.swiper.stop()
+        this.currentIndex = 0
     },
     beforeDestroy() {
-        this.swiper && this.swiper.destroy()
+        const itemEles = this.$refs.swiperContent.children
+        const transitionEvent = this.checkTransitionEvent()
+        itemEles[0] &&
+            itemEles[0].removeEventListener(transitionEvent, this.transitionEndHandler, false)
     }
 }
 </script>
@@ -244,10 +380,25 @@ export default {
     overflow: hidden;
 
     .swiper-content {
+        overflow: hidden;
         position: relative;
-        > div {
-            float: left;
-            font-size: 0;
+        .swiper-item {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            font-size: 30px;
+            background-color: gray;
+            color: #fff;
+            transition: all 0 ease 0s;
+            transform: translate3d(0, 0, 0);
+
+            img {
+                display: block;
+                width: 100%;
+                height: 100%;
+            }
         }
 
         .swiper-txt {
@@ -299,10 +450,6 @@ export default {
         }
     }
 
-    .swiper-transition {
-        transition: all 0.5s ease 0s;
-    }
-
     .swiper-prev,
     .swiper-next {
         position: absolute;
@@ -323,12 +470,6 @@ export default {
 
     .swiper-next {
         right: 5%;
-    }
-
-    .clearfix::after {
-        display: block;
-        content: '';
-        clear: both;
     }
 }
 </style>
